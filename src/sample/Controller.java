48ead007ac52eecf6e658c3347c5ed4e87117008
charl <charliecidraque@gmail.com>
2019-05-20 15:33:13 +0200

    c1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -17,6 +17,9 @@ public class Controller {
     private Label lblStatusR;
 
     @FXML
+    private TextField txtNom;
+
+    @FXML
     private TextField txtUsername;
 
     @FXML
@@ -32,7 +35,7 @@ public class Controller {
     private TextField txtPasswordR2;
 
     public void Login() throws Exception{
-        if(txtUsername.getText().equals("user") && txtPassword.getText().equals("pass")) {
+        if(txtUsername.getText().equals("user") && txtPassword.getText().equals("pass")) { //comprovacio user i pssw
             lblStatus.setText("Login Succes");
             Stage primaryStage = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
@@ -45,7 +48,7 @@ public class Controller {
         }
     }
 
-    public void Signup() throws Exception{
+    public void Signup() throws Exception{ //metode associat al "click" del boto Sign Up de la pantalla inicial
             Stage primaryStage = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("Registrar.fxml"));
             primaryStage.setScene(new Scene(root, 400, 400));
@@ -53,14 +56,20 @@ public class Controller {
             primaryStage.show();
         }
 
-    public void Signup2() throws Exception{
+    public void Signup2() throws Exception{ //metode associat al "click" del boto Sign Up en la pantalla de Sign Up
         if(!txtUsernameR.getText().equals("user") && (txtPasswordR1.getText().equals(txtPasswordR2.getText()) )) {
-        lblStatusR.setText("Sign up Succes");
-        Stage primaryStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setScene(new Scene(root, 400, 400));
-        primaryStage.setTitle("Ajedrez PROP");
-        primaryStage.show();
+            if(txtNom.getText().isEmpty() || txtPasswordR1.getText().isEmpty() || txtUsernameR.getText().isEmpty()){
+                lblStatusR.setText("Cal omplir tots els camps");
+            }
+            else {
+                lblStatusR.setText("Sign up Succes");
+                Stage primaryStage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+                primaryStage.setScene(new Scene(root, 400, 400));
+                primaryStage.setTitle("Ajedrez PROP");
+                primaryStage.show();
+            }
+
         }
         else if (txtUsernameR.getText().equals("user")) {//dir que el username user ja esta en us
             lblStatusR.setText("Aquest Username ja està en us");
@@ -69,4 +78,36 @@ public class Controller {
             lblStatusR.setText("la contrasenya ha de coincidir");
         }
     }
+
+    public void Jugar() throws Exception{
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("Jugar.fxml"));
+            primaryStage.setScene(new Scene(root, 400, 400));
+            primaryStage.setTitle("Ajedrez PROP (Jugar)");
+            primaryStage.show();
+    }
+
+    public void GestionarProblema() throws Exception{
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("GestionarProblema.fxml"));
+            primaryStage.setScene(new Scene(root, 400, 400));
+            primaryStage.setTitle("Ajedrez PROP (Gestió Problema)");
+            primaryStage.show();
+    }
+
+    public void EditarPerfil() throws Exception{
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("EditarPerfil.fxml"));
+            primaryStage.setScene(new Scene(root, 400, 400));
+            primaryStage.setTitle("Ajedrez PROP");
+            primaryStage.show();
+    }
+
+    public void ConsultarRankings() throws Exception{
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("ConsultarRankings.fxml"));
+            primaryStage.setScene(new Scene(root, 400, 400));
+            primaryStage.setTitle("Ajedrez PROP");
+            primaryStage.show();
+        }
 }
